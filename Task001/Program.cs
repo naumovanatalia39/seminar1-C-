9cc7b578aafae541c61bf825eb1d21e5188ecf79
naumovanatalia <naumova.na43@mail.ru>
2022-12-02 21:08:36 +0200

    add home task2 (Task 003)

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -3,9 +3,9 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введитe второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 if (a == b * b)
-    {
+{
     Console.WriteLine("yes");
-    }
+}
 else
 {
     Console.WriteLine("no");
---
+++ Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = " + a);
+    Console.Write(" min = " + b);
+}
+else
+{
+    Console.Write("max = " + b);
+    Console.Write(" min = " + a);
+}
