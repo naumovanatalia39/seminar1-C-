6443f1ffb13d0ea4725f564d4589fcf0c415c787
naumovanatalia <naumova.na43@mail.ru>
2022-12-02 21:23:10 +0200

    add hometask04

---
+++ Hometask04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine(a);
+}
+if (b > a && b > c)
+{
+    Console.WriteLine(b);
+}
+if (c > b && c > a)
+{
+    Console.WriteLine(c);
+}
