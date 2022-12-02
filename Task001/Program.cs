d82c26212efd0944df806932305e119c49104bee
naumovanatalia <naumova.na43@mail.ru>
2022-12-02 19:48:02 +0200

    add task001

---
+++ Task001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введитe первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введитe второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+    {
+    Console.WriteLine("yes");
+    }
+else
+{
+    Console.WriteLine("no");
+}
